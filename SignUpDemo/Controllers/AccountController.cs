6e04dcfd7b0252889e51de3e45046257b349262a
Pallavi <Pallavi@DESKTOP-8F284K2>
2021-07-10 17:30:48 +0530

    Add project files.

---
+++ SignUpDemo/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SignUpDemo.Models;
+
+namespace SignUpDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Signup(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+
+            }
+            userModel = userModel.Terms == null ? new UserModel() { Terms = "True" } : userModel;
+            return View();
+        }
+        public IActionResult Message()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ SignUpDemo/Models/UserModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUpDemo.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Please enter your Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Please set your Password")]
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match")]
+        [Display(Name ="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="Please enter Confirm Password") ]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage ="Please enter contact no")]
+        [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Please select country name")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Please select your city")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please select Gender")]
+        public string Gender { get; set; }
+
+        [RegularExpression(@"^true", ErrorMessage = "Please accept terms and conditions")]
+        public string Terms { get; set; }
+    }
+}
